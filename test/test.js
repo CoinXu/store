dd521d158ceba18bc33300f5882b7c4eee395e25
Coin <duanxian0403@163.com>
2017-04-29 17:34:06 +0800

    更新文档

--- test/test.js
+++ test/test.js
@@ -31,7 +31,18 @@ const mw = storeModelCreator([{
 
 window.store = store
 store.subscribe((next) => console.log(JSON.stringify(next)))
+
+// 添加model
+// model中不会做太多的业务数据处理，保持比较纯粹的数据请求->处理失败或成功->通知观察者
+// 这样简单的逻辑
+// 至于为什么这样做，当然是为了model共用！
+// 比如定义了数据源model之后，其他模块都不必再定义了
+// 要拿数据，调用 store.use(dataSource) 之后
+// store.dispatch({type: 'getDataSourceList'})
+// 即可
+
 store.use(mw)
+
 // 假设我们在fetch函数中添加了一个观察者
 // 每次发送请求的时候通知观察者
 // 那么我们可以在每次执行actions去确定当前是否为loading状态
@@ -39,4 +50,24 @@ store.use(mw)
 store.use(function (action, state, next) {
   next({ loading: new Date().getTime() % 2 === 0 })
 })
+
+// 为了达到view中完全不处理业务逻辑的目的
+// 我们还可以再添加一个viewModel的中间件
+// 不改变model，view中不处理数据
+// 还可以实现一份model对不同的view的定制
+store.use(function (action, state, next) {
+  const { count } = state.mod
+  const viewModel = { tips: '' }
+  
+  if (count > 10000) {
+    viewModel.tips = '骚年别玩了，手不累吗？'
+  }
+  
+  if (count < 0) {
+    viewModel.tips = '都为负了还按个啥！'
+  }
+  
+  next({ ...state, viewModel })
+})
+
 store.initialize()
