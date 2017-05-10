6c7f54264f8fe37d1f7160babb42813083a3d81d
Coin <duanxian0403@163.com>
2017-05-10 09:46:15 +0800

    添加注释

--- src/middleware/store-view-model/ViewModel.js
+++ src/middleware/store-view-model/ViewModel.js
@@ -4,6 +4,13 @@
 
 import Model from '../store-model/Model'
 
+/**
+ * @class
+ * `ViewModel`与`Model`的唯一区别就是多了一个`store`属性。
+ * 因为`Model`为了保持其纯粹性，每个`Model`都是独立存在的，不必依赖于某一个`Store`。
+ * 而`ViewModel`中包含了诸多的业务逻辑，一定是与某一个具体的`Store`相关。
+ * 而且在任何时候都需要有`dispatch`的能力。
+ */
 class ViewModel extends Model {
   /**
    * @override
