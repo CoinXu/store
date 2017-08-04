b8a9a7cfd2d1fc3cfb57a8f632abaacd833c90fe
coinxu <duanxian0605@gmail.com>
2017-08-04 14:35:39 +0800

    update readme.md

--- example/with-react/controller/actions.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created on 30/04/2017.
- */
-
-
-export default {
-  update: 'update',
-  create: 'created'
-}
--- example/with-react/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created on 30/04/2017.
- */
-
-import View from './view/view'
-import ReactDOM from 'react-dom'
-
-ReactDOM.render(<View/>, document.getElementById('container'))
