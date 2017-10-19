961815d5f99d4842deb22bbbc79af01c679aea98
coinxu <duanxian0605@gmail.com>
2017-10-19 16:41:23 +0800

    open all test case

--- __test__/index.js
+++ __test__/index.js
@@ -2,8 +2,8 @@
  * Created on 01/05/2017.
  */
 require('babel-register')
-//require('./store')
-//require('./store-model')
-//require('./store-collection')
-//require('./store-view-model')
+require('./store')
+require('./store-model')
+require('./store-collection')
+require('./store-view-model')
 require('./decorate/validator')
